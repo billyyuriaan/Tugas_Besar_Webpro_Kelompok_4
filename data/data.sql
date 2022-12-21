7f791c590e97da24822a458ad07258fd9f632106
= <=>
2022-12-21 01:51:12 -0800

    add data sql

---
+++ data/data.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS user (
+    userId INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255),
+    password VARCHAR(255),
+    PRIMARY KEY (userId)
+);
+
+
+CREATE TABLE IF NOT EXISTS donate (
+  userId INT NOT NULL,
+  donateTo VARCHAR(255),
+  ammout INT,
+  message VARCHAR(255),
+  alias VARCHAR(255),
+  payMethode VARCHAR(255),
+  donateDate VARCHAR(255),
+  FOREIGN KEY (userId) REFERENCES user(userId)
+);
+
+
+INSERT INTO user (userId,email,password) VALUES (1,"dummy@email.com", "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8");
+INSERT INTO user (userId,email,password) VALUES (2,"wow@email.com", "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8");
+
+INSERT INTO donate (userId, donateTo, ammout, message, alias, payMethode, donateDate) VALUES (1, "wow", 20000,"SALAM DARI AKU", "kamu", "GOPAY", "1671614657360");
+INSERT INTO donate (userId, donateTo, ammout, message, alias, payMethode, donateDate) VALUES (1, "dia", 50000,"SALAM DARI AKU", "kamu", "GOPAY", "1671614657360");
+INSERT INTO donate (userId, donateTo, ammout, message, alias, payMethode, donateDate) VALUES (1, "he", 100000,"SALAM DARI AKU", "kamu", "GOPAY", "1671614657360");
+INSERT INTO donate (userId, donateTo, ammout, message, alias, payMethode, donateDate) VALUES (1, "she", 200000,"SALAM DARI AKU", "kamu", "GOPAY", "1671614657360");
+
+INSERT INTO donate (userId, donateTo, ammout, message, alias, payMethode, donateDate) VALUES (2, "she", 20000,"SALAM DARI AKU", "kamu", "GOPAY", "1671614657360");
+INSERT INTO donate (userId, donateTo, ammout, message, alias, payMethode, donateDate) VALUES (2, "he", 50000,"SALAM DARI AKU", "kamu", "GOPAY", "1671614657360");
+INSERT INTO donate (userId, donateTo, ammout, message, alias, payMethode, donateDate) VALUES (2, "dia", 100000,"SALAM DARI AKU", "kamu", "GOPAY", "1671614657360");
+INSERT INTO donate (userId, donateTo, ammout, message, alias, payMethode, donateDate) VALUES (2, "wow", 200000,"SALAM DARI AKU", "kamu", "GOPAY", "1671614657360");
